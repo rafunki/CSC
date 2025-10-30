c35efcba90e9b40695521ba3ac17b96aa386e92e
rafunki <rafaelare03@gmail.com>
2025-10-30 10:52:55 -0600

    Initial Commit

---
+++ CSC/CSCApp.swift
@@ -0,0 +1,32 @@
+//
+//  CSCApp.swift
+//  CSC
+//
+//  Created by Rafael Nuñez on 30/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CSCApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
